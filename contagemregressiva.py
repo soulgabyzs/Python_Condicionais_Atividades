6eaf5630005455032df6bd68422d33ddc8d826ff
Gabriela Monterosso <138223025+soulgabyzs@users.noreply.github.com>
2024-07-30 09:55:40 -0300

    Contagem Regressiva
    Programa que exibe uma contagem regressiva de 10 até 1 usando a estrutura while.

---
+++ contagemregressiva.py
@@ -0,0 +1,6 @@
+print("A contagem regressiva irá começar...")
+contador = 10
+while contador > 0:
+    print(contador)
+    contador -= 1
+print("Fim da contagem regressiva, agora o computador irá explodir <3!")
